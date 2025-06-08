7005fa07f816d6a01fd9828274fc802bdf947cd5
Saipramodh033 <saipramodhappishetty@gmail.com>
2025-06-08 13:12:47 +0530

    dependecy fix

--- my_django_backend/my_django_backend/settings.py
+++ my_django_backend/my_django_backend/settings.py
@@ -17,7 +17,6 @@ import os
 from decouple import config
 import dj_database_url
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -103,12 +102,12 @@ WSGI_APPLICATION = 'my_django_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
 
 
 # Password validation
