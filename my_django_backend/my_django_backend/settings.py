b1a81361942b40d52bb910883b14bd657c6ffc29
Saipramodh033 <saipramodhappishetty@gmail.com>
2025-07-07 14:24:42 +0530

    rate limit updated

--- my_django_backend/my_django_backend/settings.py
+++ my_django_backend/my_django_backend/settings.py
@@ -40,6 +40,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated"
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '100/day',
+        'anon': '20/hour',
+    }
 }
 
 SIMPLE_JWT = {
